852cacd40978ad52e6dd06f6f232c76682ac7358
Krishna Kumar <nnkrishna714@gmail.com>
2023-01-23 19:48:26 +0530

    Delete first.py

--- first.py
+++
@@ -1,8 +0,0 @@
-print("hello world")
-print("this is krishna")
-
-print("mango","apple","orange",sep=" ")
-print("mango","apple","orange",sep=" India\n")
-print("mango","apple","orange",sep="\nIndia ")
-print("mango","apple","orange",sep="\nIndia\n",end=" ")
-print("mango","apple","orange",sep=" ")
