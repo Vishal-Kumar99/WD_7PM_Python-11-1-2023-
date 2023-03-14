7e258ab1082d7135ab49a40b39c4799c3efdfdac
Krishna Kumar <nnkrishna714@gmail.com>
2023-03-14 18:59:56 +0530

    commit

---
+++ pycharm_pattern/Pattern.py
@@ -0,0 +1,63 @@
+
+# for i in range(5):
+#     for j in range(0,i+1):
+#         print("*",end="")
+#     print()
+
+
+# for i in range(5):
+#     for j in range(0,i+1):
+#         print("*")
+#     print()
+
+
+# for i in range(5):
+#     for j in range(0,i+1):
+#         print("*")
+
+
+# for i in range(5):
+#     for j in range(0,i):
+#         print("*",end="")
+#     print()
+
+# i=0
+# while i<5:
+#     j=0
+#     while j<(i+1):
+#         print("*",end="")
+#         j+=1
+#     print()
+#     i+=1
+
+# i=1
+# while i<6:
+#     print("*"*i)
+#     i+=1
+
+# print("*"*1)
+# print("*"*2)
+# print("*"*3)
+# print("*"*4)
+# print("*"*5)
+
+# for i in range(5,10):
+#     print("*"*(i-4))
+
+
+for i in range(1,6):
+    print(f"{i}"*i)
+#
+# for i in range(5,10):
+#     print("*"*(i-4))
+
+
+age=27
+name="krishna"
+
+print(f"I am {name} and age {age}")
+print(F"I am {name} and age {age}")
+
+print("I am {} and age {}".format(name,age))
+print("I am {} and age {}".format(age,name))
+print("I am {1} and age {0}".format(age,name))
